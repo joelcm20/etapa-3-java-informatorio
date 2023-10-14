049f20ef6f2d12cb79b77a450eb508d76c966dcb
joelcm <132691926+joelcm20@users.noreply.github.com>
2023-10-14 00:23:28 -0300

    ejercicios practicos 3: padawan java jedi(estudiante y calificaciones)

--- clases/clase06/ejercicios/padawan_java_jedi/EstudianteYCalificaciones.java
+++ clases/clase06/ejercicios/padawan_java_jedi/EstudianteYCalificaciones.java
@@ -1,8 +1,10 @@
 package ejercicios.padawan_java_jedi;
 
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class EstudianteYCalificaciones {
@@ -26,6 +28,7 @@ public class EstudianteYCalificaciones {
         menu.append("[----------APP ESTUDIANTES Y CALIFICACIONES----------]\n");
         menu.append("1. Listar Estudiantes.\n");
         menu.append("2. Agregar Estudiante.\n");
+        menu.append("3. Agregar Calificacion.\n");
         menu.append("0. Salir.\n");
         System.out.println(menu);
     }
@@ -41,6 +44,9 @@ public class EstudianteYCalificaciones {
                 break;
             case "2":
                 this.crearEstudiante(input);
+                break;
+            case "3":
+                this.crearCalificacion(input);
                 break;
             case "0":
                 continuarEjecucion = Boolean.FALSE;
@@ -60,11 +66,16 @@ public class EstudianteYCalificaciones {
             StringBuilder listaEstudiantes = new StringBuilder();
 
             for (Estudiante e : this.estudiantes) {
-                listaEstudiantes.append(String.format("ID[%S] %s | %s.\n", e.id, e.nombre, e.calcPromCalif()));
+                listaEstudiantes.append(String.format("ID[%S] %s | %s.\n", e.id, e.nombre, this.floatFormat(e.calcPromCalif())));
             }
 
             System.out.println(listaEstudiantes);
         }
+    }
+
+    private String floatFormat(float f) {
+        DecimalFormat decFormat = new DecimalFormat("#.#");
+        return decFormat.format(f);
     }
 
     private void crearEstudiante(Scanner input) {
@@ -83,12 +94,49 @@ public class EstudianteYCalificaciones {
     private boolean agregarEstudiante(Estudiante e) {
         return this.estudiantes.add(e);
     }
+
+    private void crearCalificacion(Scanner input) {
+        System.out.print("ID del estudiante >> ");
+        int idEstudiante = Integer.parseInt(input.nextLine());
+
+        final Estudiante ESTUDIANTE = this.buscarEstudiante(idEstudiante);
+        if (ESTUDIANTE == null) {
+            System.out.println("Estudiante no encontrado.\n\n");
+            return;
+        }
+
+        boolean otraCalificacion = Boolean.FALSE;
+        do {
+            System.out.printf("numero de la calificacion >> ");
+            Calificacion calificacion = new Calificacion(Float.parseFloat(input.nextLine()));
+
+            ESTUDIANTE.agregarCalificacion(calificacion);
+
+            System.out.printf("¿agregar otra calificacion? Presionar 1(si). Enter(no) >> ");
+            otraCalificacion = Objects.equals(input.nextLine(), "1") ? Boolean.TRUE : Boolean.FALSE;
+        } while (otraCalificacion);
+    }
+
+    private Estudiante buscarEstudiante(int id) {
+        Estudiante estudiante = null;
+        for (Estudiante e : this.estudiantes) {
+            if (e.id == id) {
+                estudiante = e;
+                break;
+            }
+        }
+        return estudiante;
+    }
 }
 
 class Estudiante {
     int id;
     String nombre;
     List<Calificacion> calificaciones = new ArrayList<>();
+
+    public void agregarCalificacion(Calificacion calif) {
+        this.calificaciones.add(calif);
+    }
 
     public float calcPromCalif() {
         if (this.calificaciones.isEmpty()) {
@@ -106,4 +154,9 @@ class Estudiante {
 class Calificacion {
     float numero;
     LocalDate fecha;
+
+    public Calificacion(float numero) {
+        this.numero = numero;
+        this.fecha = LocalDate.now();
+    }
 }
